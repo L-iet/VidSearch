fce769f3d99f9276dd9cc24a1b5705eca65973a2
Joshua <joshjohnmark@gmail.com>
2022-07-11 11:35:07 +0100

    added license

--- static/app.js
+++ static/app.js
@@ -1,3 +1,9 @@
+// Copyright (c) 2022, Joshua Mark
+// All rights reserved.
+
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. 
+
 var times = [];
 var timestr = [];
 var currInd = 0;
